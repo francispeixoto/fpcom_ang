73f48c2033141f63ac241b48cb255badd14725b8
Francis Peixoto <peixoto.francis@gmail.com>
2024-04-12 10:49:27 -0400

    initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faHandshake as fasHandshake } from '@fortawesome/free-solid-svg-icons';
+import { faRoad as fasRoad } from '@fortawesome/free-solid-svg-icons';
+import { faBook as fasBook } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope as fasEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub as fasGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin as fasLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(fasStar, fasHandshake, fasBook, fasRoad, fasEnvelope, fasGithub, fasLinkedin);}
+ }
