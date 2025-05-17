20f892fca9a3e9c63490e891c7f6f074b97940d1
Francis Peixoto <peixoto.francis@gmail.com>
2025-05-17 13:53:14 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [FontAwesomeModule]
 })
 export class AppComponent {
 }
