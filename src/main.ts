b9353a63d17c1230061835003ace97041b812713
Francis Peixoto <peixoto.francis@gmail.com>
2025-05-17 13:53:45 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope, faPhone, faHandshake, faRoad, faBook } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faEnvelope, faPhone, faHandshake, faRoad, faBook, faLinkedin);
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
