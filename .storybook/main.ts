80ec0221d7f8e789975c8f9a6573672b5162bcd8
Francis Peixoto <peixoto.francis@gmail.com>
2026-02-07 15:33:53 -0500

    chore(storybook): add Storybook config and README note

---
+++ .storybook/main.ts
@@ -0,0 +1,14 @@
+export default {
+  stories: ['../src/**/*.stories.@(ts|mdx)'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/angular',
+    options: {}
+  },
+  core: {
+    builder: '@storybook/builder-webpack5'
+  },
+  typescript: {
+    check: false
+  }
+};
